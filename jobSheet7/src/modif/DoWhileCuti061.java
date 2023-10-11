8c25615a60faf041a42d26d7396717e3615f1b51
aorysan <aryoadiputro@gmail.com>
2023-10-11 10:08:05 +0700

    modif cuti

--- jobSheet7/src/modif/DoWhileCuti061.java
+++ jobSheet7/src/modif/DoWhileCuti061.java
@@ -18,13 +18,16 @@ public class DoWhileCuti061 {
             if (konfirmasi.equalsIgnoreCase("y")) {
                 System.out.print("Jumlah hari : ");
                 jumlahHari = sc.nextInt();
-
+            
+            
                 if (jumlahHari <= jatahCuti) {
                     jatahCuti -= jumlahHari;
                     System.out.println("Sisa jatah cuti : " + jatahCuti);
                 } else {
                     System.out.println("Sisa jatah cuti Anda tidak mencukupi");
                 }
+            } else if (konfirmasi.equalsIgnoreCase("t")) {
+                break;
             }
         } while (jatahCuti > 0);
 
