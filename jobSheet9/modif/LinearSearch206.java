6bc505e9435c5765e4d63a2a617419d4d26d4410
aorysan <aryoadiputro@gmail.com>
2023-10-25 10:48:55 +0700

    pertanyaan linear search

---
+++ jobSheet9/modif/LinearSearch206.java
@@ -0,0 +1,35 @@
+package jobSheet9.modif;
+
+import java.util.*;
+public class LinearSearch206 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+        int key, hasil = 0, p;
+
+        System.out.printf("Masukkan jumlah elemen array : ");
+        p = sc.nextInt();
+
+        int arrayInt[] = new int[p];
+        for (int i = 0; i < arrayInt.length; i++) {
+            System.out.printf("Masukkan elemen array ke-%d : ", i);
+            arrayInt[i] = sc.nextInt();
+        }
+
+        System.out.println("Masukkan key yang ingin anda cari : ");
+        key = sc.nextInt();
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i]==key) {
+                hasil = i;
+            }
+        }
+
+        if (arrayInt[hasil]==key) {
+            System.out.printf("Key ada dalam array pada posisi indeks ke-%d", hasil);
+        } else {
+            System.out.println("Key yang anda cari tidak ada dalam array");
+        }
+        sc.close();
+    }
+}
