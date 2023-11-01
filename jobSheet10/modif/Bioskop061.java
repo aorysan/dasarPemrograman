1a418b32f42bac1a2b716af5ac30255e15deb8fd
aorysan <aryoadiputro@gmail.com>
2023-11-01 08:13:34 +0700

    percobaan 1 pertanyaan 10

--- jobSheet10/modif/Bioskop061.java
+++ jobSheet10/modif/Bioskop061.java
@@ -23,15 +23,15 @@ public class Bioskop061 {
         //     System.out.println("Panjang baris ke-" + (i+1) + " : " + penonton.length);
         // }
 
-        System.out.println(penonton.length);
-        for (String[] barisPenonton : penonton) {
-            System.out.println("Panjang baris : " + barisPenonton.length);
-        }
-
-        // System.out.println("Penonton baris ke-3: ");
-        // for (int i = 0; i < penonton[2].length; i++) {
-        //     System.out.println(penonton[2][i]);
+        // System.out.println(penonton.length);
+        // for (String[] barisPenonton : penonton) {
+        //     System.out.println("Panjang baris : " + barisPenonton.length);
         // }
+
+        System.out.println("Penonton baris ke-3: ");
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
 
         // System.out.println("Penonton pada baris ke-3: ");
         // for (String i : penonton[2]) {
