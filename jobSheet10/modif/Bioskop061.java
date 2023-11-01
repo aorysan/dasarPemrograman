1c72f291d2e92971c0e336424ddd703f8f69064f
aorysan <aryoadiputro@gmail.com>
2023-11-01 08:19:18 +0700

    percobaan 1 pertanyaan 12

--- jobSheet10/modif/Bioskop061.java
+++ jobSheet10/modif/Bioskop061.java
@@ -33,13 +33,13 @@ public class Bioskop061 {
         //     System.out.println(penonton[2][i]);
         // }
 
-        System.out.println("Penonton pada baris ke-3: ");
-        for (String i : penonton[2]) {
-            System.out.println(i);
-        }
-
-        // for (int i = 0; i < penonton.length; i++) {
-        //     System.out.println("Penonton pada baris ke-" + (i+1) + " : " + String.join(", ", penonton[i]));
+        // System.out.println("Penonton pada baris ke-3: ");
+        // for (String i : penonton[2]) {
+        //     System.out.println(i);
         // }
+
+        for (int i = 0; i < penonton.length; i++) {
+            System.out.println("Penonton pada baris ke-" + (i+1) + " : " + String.join(", ", penonton[i]));
+        }
     }
 }
