ab4a619868e78aec0c337027e1254486e074e049
aorysan <aryoadiputro@gmail.com>
2023-11-08 08:45:46 +0700

    jobSheet11

--- jobSheet11/src/NestedLoop_2341720084.java
+++ jobSheet11/src/NestedLoop_2341720084.java
@@ -9,7 +9,7 @@ public class NestedLoop_2341720084 {
         double temps[][] = new double[5][7];
     
         for(int i = 0; i < temps.length; i++) {
-            System.out.print("Kota ke-" + i);
+            System.out.println("Kota ke-" + i);
             for(int j = 0; j < temps[0].length; j++) {
                 System.out.print("Hari ke-" + (j+1) + " : ");
                 temps[i][j] = sc.nextDouble();
