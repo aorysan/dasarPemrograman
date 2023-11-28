1cb257562965aa0c7aa4b09fba0f8b96395b7e75
aorysan <aryoadiputro@gmail.com>
2023-11-28 18:01:41 +0700

    jobSheet14

---
+++ jobSheet14/src/Percobaan2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Percobaan2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Bilangan yang dihitung : ");
+        int bilangan = sc.nextInt();
+        System.out.print("Pangkat : ");
+        int pangkat = sc.nextInt();
+
+        System.out.println(hitungPangkat(bilangan, pangkat));
+
+        sc.close();
+    }
+    static int hitungPangkat (int x, int y) {
+        if (y == 0) {
+            return (1);
+        } else {
+            return (x * hitungPangkat(x, y-1));
+        }
+    }
+}
